0c522b3a1401a6c2b389ec91cde8d11603a425e5
SoutPanhavon168 <Panhavon.sout@student.cadt.edu.kh>
2026-03-26 12:42:51 +0700

    initial commit

---
+++ lib/data/dtos/song_dto.dart
@@ -0,0 +1,27 @@
+import '../../model/songs/song.dart';
+
+class SongDto {
+  static const String titleKey = 'title';
+  static const String artistIdKey = 'artistId';
+  static const String durationKey = 'duration';
+  static const String imageUrlKey = 'imageUrl';
+
+  static Song fromJson(Map<String, dynamic> json, String id) {
+    return Song(
+      id: id, // ✅ comes from Firebase key
+      title: json[titleKey],
+      artistId: json[artistIdKey],
+      duration: Duration(milliseconds: json[durationKey]),
+      imageUrl: Uri.parse(json[imageUrlKey]),
+    );
+  }
+
+  Map<String, dynamic> toJson(Song song) {
+    return {
+      titleKey: song.title,
+      artistIdKey: song.artistId,
+      durationKey: song.duration.inMilliseconds,
+      imageUrlKey: song.imageUrl.toString(),
+    };
+  }
+}
